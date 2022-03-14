27e85876b6c8fa0026f05bea33ceb1c9874743dc
SadiLuis <euumemo2013@gmail.com>
2022-03-14 15:18:55 -0300

    Componentes basic

---
+++ Screens/OnLoadingHome.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const OnLoadingHome = () => {
+  return (
+    <View>
+      <Text>OnLoadingHome</Text>
+    </View>
+  )
+}
+
+export default OnLoadingHome
